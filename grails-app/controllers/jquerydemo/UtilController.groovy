f15c36c4fcd593acecbca0d1e8a0612af46e4278
Manish <manish@intelligrape.com>
2015-02-26 17:41:49 +0530

    Added demo for $.ajax()

--- grails-app/controllers/jquerydemo/UtilController.groovy
+++ grails-app/controllers/jquerydemo/UtilController.groovy
@@ -1,5 +1,7 @@
 package jquerydemo
 
+import grails.converters.JSON
+
 import com.google.gson.JsonObject
 import grails.converters.JSON
 
@@ -24,5 +26,24 @@ class UtilController {
     }
 
     def JSONDemo(){
+
+    }
+
+    def ajaxDemo(){
+
+    }
+
+    def randomName(){
+        Boolean isAllowed = params.boolean('isAllowed')
+        List<String> names = ['Rohan', 'Vivek','Rohit','Puneet','Roni','Jeevesh']
+        String errorMessage
+        throw new Exception("hahahaha")
+        if(isAllowed){
+            String name = names.get(new Random().nextInt(names.size()))
+            render(template: 'name', model: [name:name])
+        }else{
+            errorMessage = 'You are not allowed to see the data'
+            render([status: false, errorMessage:errorMessage] as JSON)
+        }
     }
 }
